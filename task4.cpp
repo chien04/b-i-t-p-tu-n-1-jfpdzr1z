25624b15c1d3f86a42350f999b02f902141e89f9
chienbv <chienlgend24032004@gmail.com>
2026-05-30 16:28:01 +0700

    Submit task 4 - Score: 3.0

--- task4.cpp
+++ task4.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // Your code here
+    string s;
+    getline(cin, s);
+
+    int left = 0;
+    int right = s.length() - 1;
+
+    bool palindrome = true;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            palindrome = false;
+            break;
+        }
+
+        left++;
+        right--;
+    }
+
+    if (palindrome) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
     return 0;
 }
