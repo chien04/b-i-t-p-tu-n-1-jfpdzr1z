97f01c37339776350032e6bb6f04e1d8e2669163
chienbv <chienlgend24032004@gmail.com>
2026-05-30 16:28:35 +0700

    Submit task 3 - Score: 3.0

--- task3.cpp
+++ task3.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main() {
-    // Your code here
+    int n;
+    cin >> n;
+
+    map<int, int> freq;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    for (auto p : freq) {
+        cout << p.first << " " << p.second << endl;
+    }
+
     return 0;
 }
