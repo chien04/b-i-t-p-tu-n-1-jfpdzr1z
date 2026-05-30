6f309e2602108b2bb37b0563be56fae6d5a90f38
chienbv <chienlgend24032004@gmail.com>
2026-05-30 16:27:38 +0700

    Submit task 1 - Score: 2.0

--- task1.cpp
+++ task1.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    // Your code here
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+
+    for (int x : a) {
+        cout << x << " ";
+    }
+
     return 0;
 }
